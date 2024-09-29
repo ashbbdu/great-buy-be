2191fedd4991afa2926ba8fb4f9aa7215581f247
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-09-29 21:36:57 +0530

    init

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ require("dotenv").config;
 const app = express()
 
 app.use(express.json())
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 
 connect()
+
+app.get("/" , (req , res) => {
+    res.send("App is up and running")
+})
 app.use("/api/v1/auth" , authRoutes )
 
 app.listen(PORT , () => {
